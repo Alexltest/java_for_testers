d5b1248c56ab534395e663fa33e12efc12f1b41c
AlexL <alexl@forbet.net>
2018-02-06 14:25:08 +0300

    add third task;

---
+++ sandbox/src/test/java/ru/jft/sandbox/ThirdTest.java
@@ -0,0 +1,16 @@
+package ru.jft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class ThirdTest {
+
+    @Test
+    public void testArea() {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+
+    }
+}
