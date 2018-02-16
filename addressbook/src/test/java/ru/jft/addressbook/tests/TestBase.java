5196cb3b6fc008260bf434eabdc2a5668d6a66e7
AlexL <alexl@forbet.net>
2018-02-16 17:37:49 +0300

    7 task;

--- addressbook/src/test/java/ru/jft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/TestBase.java
@@ -1,15 +1,16 @@
 package ru.jft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.jft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() {
         app.init();
     }
 
