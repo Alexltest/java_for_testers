4031a747b64e8812ab0f4ad2dc491de696c291ae
AlexL <alexl@forbet.net>
2018-02-21 10:26:14 +0300

    fix name of the group;

--- addressbook/src/test/java/ru/jft/addressbook/tests/ContactModificationTests.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/ContactModificationTests.java
@@ -8,7 +8,7 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification () throws InterruptedException {
         if (! app.getContactHelper().isThereAContact()) {
-            app.getContactHelper().CreateContact(new ContactData("First", "Middle", "Last", "Alex", "Vgr", "Russia", "867575", "sbs@example.com", null), true);
+            app.getContactHelper().CreateContact(new ContactData("First", "Middle", "Last", "Alex", "Vgr", "Russia", "867575", "sbs@example.com", "test"), true);
         }
         app.getContactHelper().selectContact();
         app.getContactHelper().initContactModification();
