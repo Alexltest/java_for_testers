2165c8af8257cf631570832cd1366ee8b5b6c7d1
AlexL <alexl@forbet.net>
2018-02-26 17:47:45 +0300

    group lists;

--- addressbook/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,20 @@
 package ru.jft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.jft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().CreateGroup(new GroupData("test", null, null));
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
--- addressbook/src/test/java/ru/jft/addressbook/tests/GroupModificationTests.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/GroupModificationTests.java
@@ -1,20 +1,26 @@
 package ru.jft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.jft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
+        if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().CreateGroup(new GroupData("test", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test", "header", "footer"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
