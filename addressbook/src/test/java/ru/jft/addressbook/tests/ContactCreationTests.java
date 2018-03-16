3baa94fe17250773936ee74ffbcb401001e2674e
AlexL <alexl@forbet.net>
2018-03-16 16:39:39 +0300

    add logs

--- addressbook/src/test/java/ru/jft/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/ContactCreationTests.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
