953edd69dff4f41bddb73598cc7b478d2425ec51
AlexL <alexl@forbet.net>
2018-03-20 13:30:43 +0300

    change ContactDeleteTests

--- addressbook/src/test/java/ru/jft/addressbook/tests/ContactDeleteTests.java
+++ addressbook/src/test/java/ru/jft/addressbook/tests/ContactDeleteTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.jft.addressbook.model.ContactData;
 import ru.jft.addressbook.model.Contacts;
+import ru.jft.addressbook.model.Groups;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -11,6 +13,7 @@ public class ContactDeleteTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
+        Groups groups = app.db().groups();
         if (app.db().contacts().size() == 0) {
             app.contact().create(new ContactData()
                     .withFirstname("Alex")
@@ -22,7 +25,7 @@ public class ContactDeleteTests extends TestBase {
                     .withEmail2("wv@va.ru")
                     .withEmail3("wv@va.ru")
                     .withAddress("adadad")
-                    //.withGroup("test")
+                    .inGroup(groups.iterator().next())
                     , true);
         }
     }
