6d50bfec702836cb4ce6faa74281fd18dba5259d
AlexL <alexl@forbet.net>
2018-03-23 14:38:57 +0300

    add MailHelper

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -7,3 +7,5 @@ $g_db_password = '';
 $g_default_timezone = 'Europe/Berlin';
 $g_crypto_master_salt = '4hzMfr1vPKIJT7S0msgYQh8u7StO+cwvWkPS6a5I9Ao=';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
